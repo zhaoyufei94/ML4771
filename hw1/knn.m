dc4490c7c7d0dd4371ee7d2d6f6d42c1032fbcdf
Yufei Zhao <yz3170@columbia.edu>
2018-02-10 17:55:43 -0500

    finish

--- hw1/knn.m
+++ hw1/knn.m
@@ -1,25 +1,27 @@
-function accuracy = knn(train_size, start_point, k, X, Y)
+function accuracy = knn(train_size, k, X, Y)
 % Compute accuracy of k-NN on given data
 % train_size: number of data(out of 10000) used to train k-NN
 % k: parameter in k-NN
 % X, Y: data (10000*784) and label (10000*1)
-tic;
+
 correct = 0;
+tt = 0;
 
-if train_size + start_point < 10000
-for i = train_size + start_point: 10000
+for i = train_size + 1: 10000
     d = zeros(1, train_size);
     count = zeros(1, 10);
     
-    for j = start_point: train_size + start_point - 1
+    for j = 1: train_size
+        tic;
         %dd = (X(i, :) - X(j, :)) * (X(i, :) - X(j, :))';
         %d(j - start_point + 1) = sqrt(dd);
-        d(j - start_point + 1) = norm((X(i, :) - X(j, :)), Inf);
+        d(j) = norm((X(i, :) - X(j, :)));
+        tt = tt + toc;
     end
 
     [~, n] = sort(d);
     for j = 1:k
-        count(Y(n(j) + start_point - 1) + 1) = count(Y(n(j) + start_point - 1) + 1) + 1;
+        count(Y(n(j)) + 1) = count(Y(n(j)) + 1) + 1;
     end
     [~, n] = sort(count, 'descend');
     if n(1) == Y(i) + 1
@@ -27,31 +29,6 @@ for i = train_size + start_point: 10000
     end
 
 end
-end
+disp(tt/(train_size*(10000-train_size)))
 
-if start_point > 1
-for i = 1: start_point - 1
-    d = zeros(1, train_size);
-    count = zeros(1, 10);
-    
-    for j = start_point: train_size + start_point - 1
-        %dd = (X(i, :) - X(j, :)) * (X(i, :) - X(j, :))';
-        %d(j - start_point + 1) = sqrt(dd);
-        d(j - start_point + 1) = norm((X(i, :) - X(j, :)), Inf);
-    end
-
-    [~, n] = sort(d);
-    for j = 1:k
-        count(Y(n(j) + start_point - 1) + 1) = count(Y(n(j) + start_point - 1) + 1) + 1;
-    end
-    [~, n] = sort(count, 'descend');
-    if n(1) == Y(i) + 1
-        correct = correct + 1;
-    end
-
-end
-end
-t = toc;
 accuracy = correct / (10000 - train_size);
-disp(accuracy);
-disp(t / 60);
--- hw1/mle.m
+++ hw1/mle.m
@@ -1,4 +1,4 @@
-function accuracy = mle(train_size, start_point, XX, Y)
+function accuracy = mle(train_size, XX, Y)
 % Compute accuracy of MLE Multiple Gaussian on given data
 % train_size: number of data(out of 10000) used to train Multiple Gaussian MLE
 % XX: preprocessed training & test data (10000 * 200)
@@ -12,7 +12,7 @@ s_1 = zeros(1, 10);
 s_2 = zeros(200, 200, 10);
 accuracy = -1;
 
-for i = start_point :train_size + start_point - 1
+for i = 1: train_size
     n(Y(i) + 1) = n(Y(i) + 1) + 1;
     mu(Y(i) + 1, :) = mu(Y(i) + 1, :) + XX(i, :);
 end
@@ -21,7 +21,7 @@ for i = 1:10
     PY(i) = n(i) / train_size;
 end
 
-for i = start_point: train_size + start_point - 1
+for i = 1: train_size
     sigma(:, :, Y(i) + 1) = sigma(:, :, Y(i) + 1) + (XX(i, :) ...
         - mu(Y(i) + 1, :))' * (XX(i, :) - mu(Y(i) + 1, :));
 end
@@ -33,8 +33,7 @@ for i = 1: 10
 end
 
 correct = 0;
-if start_point + train_size < 10000
-for i = train_size + start_point: 10000
+for i = train_size + 1: 10000
     max_P = -Inf;
     class = -1;
     for j = 1: 10
@@ -56,31 +55,7 @@ for i = train_size + start_point: 10000
         correct = correct + 1;
     end
 end
-end
-if start_point > 1
-for i = 1: start_point - 1
-    max_P = -Inf;
-    class = -1;
-    for j = 1: 10
-        %PXY = exp(-(XX(i, :) - mu(j, :)) * sigma(:, :, j)^-1 * ...
-        %    (XX(i, :) - mu(j, :))'/2) / sqrt((2*pi)^200 * det(sigma(:, :, j)));
-        logPXY = -(log(s_1(j)) + (XX(i,:) - mu(j,:)) * ...
-            s_2(:, :, j) * (XX(i, :) - mu(j, :))') / 2;
-        % arg max PXY * PY = arg max logPXY + log(PY)
-        if logPXY + log(PY(j)) > max_P
-            max_P = logPXY + log(PY(j));
-            class = j - 1;
-        end
-    end
-    if class < 0
-        disp('error');
-        return;
-    end
-    if class == Y(i)
-        correct = correct + 1;
-    end
-end
-end
+
 
 accuracy = correct / (10000 - train_size);
 
