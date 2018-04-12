f645493edb59293593ef0316577aa79d234ef25d
Yufei Zhao <yz3170@columbia.edu>
2018-04-12 19:26:27 -0400

    more structural codes and pictures

--- hw4/code/p4_2.py
+++ hw4/code/p4_2.py
@@ -1,4 +1,5 @@
 import numpy as np
+np.seterr(divide='ignore', invalid='ignore')
 
 Dist = np.array([[0, 206, 429, 1504, 963, 2976, 3095, 2979, 1949],
                  [206, 0, 233, 1308, 802, 2815, 2934, 2786, 1771],
@@ -10,68 +11,82 @@ Dist = np.array([[0, 206, 429, 1504, 963, 2976, 3095, 2979, 1949],
                  [2979, 2786, 2631, 2687, 2054, 1131, 379, 0, 1059],
                  [1949, 1771, 1616, 2037, 996, 1307, 1235, 1059, 0]])
 
-cities = np.random.randint(1000, size=(9, 2))
+class City:
+    def __init__(self, size=(9, 2), D=Dist):
+        self.cities = np.random.randint(1000, size=size).astype(float)
+        self.Dist = D
+        self.n, self.m = size[0], size[1]
+        print("initial score: {}".format(self.get_score()))
+
+    def distance(self, x1, x2):
+        return np.linalg.norm(x1 - x2)
+
+    def get_error(self):
+        d = np.zeros((self.n, self.n))
+        for i in range(self.n):
+            for j in range(self.n):
+                d[i, j] = self.distance(self.cities[i], self.cities[j])
+        return (abs(self.Dist - d))
+
+    def get_score(self):
+        res = 0
+        for i in range(self.n):
+            for j in range(i):
+                res += (self.distance(self.cities[i], self.cities[j]) -
+                self.Dist[i, j]) ** 2
+        return res
+
+    def derivative(self, i):
+        res = np.zeros((1, self.m))
+        for j in range(self.n):
+            if i == j:
+                continue
+            d = self.distance(self.cities[i], self.cities[j])
+            res += ((d - self.Dist[i, j]) / d) * (self.cities[i] - self.cities[j])
+        return res
+
+    def update(self):
+        delta = np.zeros((self.n, self.m))
+        for i in range(self.n):
+            delta[i] = 0.01 * self.derivative(i)
+        self.cities -= delta
+
+city = City()
 
 
-def distance(x1, x2):
-    return np.linalg.norm(x1 - x2)
+for i in range(2000):
+    city.update()
+    if not i % 50:
+        print("epoch: {}, score: {}".format(i, city.get_score()))
+
+print("distance error")
+print(city.get_error())
+err = city.get_error()
+err_portion = np.divide(err.astype(float), city.Dist.astype(float))
+for i in range(9):
+    err_portion[i, i]=0
+print("portional error")
+print(err_portion)
+for i in range(9):
+    err_portion[i, i]=0
+print("mean protional error: {}".format(err_portion.mean()))
+
+print("city coordinates:")
+print(city.cities)
 
 
-def display(x, D):
-    n = D.shape[0]
-    d = np.zeros((n, n))
-    for i in range(n):
-        for j in range(n):
-            d[i, j] = distance(x[i], x[j])
-    # return (abs((D - d)).astype(int))
-    return (abs((D - d)))
+import matplotlib.pyplot as plt
+def plot():
+    plt.scatter(city.cities[0, 0], city.cities[0, 1], c='m', label='BOS')
+    plt.scatter(city.cities[1, 0], city.cities[1, 1], marker='o', c='r', label='NYC')
+    plt.scatter(city.cities[2, 0], city.cities[2, 1], marker='x', c='c', label='DC')
+    plt.scatter(city.cities[3, 0], city.cities[3, 1], c='y', label='MIA')
+    plt.scatter(city.cities[4, 0], city.cities[4, 1], c='b', label='CHI')
+    plt.scatter(city.cities[5, 0], city.cities[5, 1], c='g', label='SEA')
+    plt.scatter(city.cities[6, 0], city.cities[6, 1], c='k', label='SF')
+    plt.scatter(city.cities[7, 0], city.cities[7, 1], c='c', label='LA')
+    plt.scatter(city.cities[8, 0], city.cities[8, 1], marker='x', c='m', label='DEN')
+    plt.legend()
+    plt.show()
 
-
-display(cities, Dist)
-
-
-def derivative(x, D, i):
-    res = np.zeros((1, 2))
-    n = D.shape[0]
-    for j in range(n):
-        if i == j:
-            continue
-        d = distance(x[i], x[j])
-        res += ((d - D[i, j]) / d) * (x[i] - x[j])
-    return 2. * res
-
-
-def update(x, D):
-    n, m = x.shape[0], x.shape[1]
-    dx = np.zeros((n, m))
-    for i in range(n):
-        dx[i] = 0.01 * derivative(x, D, i)
-    return x - dx
-
-
-def get_score(x, D):
-    n = x.shape[0]
-    res = 0
-    for i in range(n):
-        for j in range(i):
-            res += (distance(x[i], x[j]) - D[i, j]) ** 2
-    return res
-
-
-cities = np.random.randint(1000, size=(9, 2))
-get_score(cities, Dist)
-
-for i in range(1000):
-    cities = update(cities, Dist)
-    if not i % 10:
-        print("epoch: {}, score: {}".format(i, get_score(cities, Dist)))
-
-get_score(cities, Dist)
-err = display(cities, Dist)
-np.seterr(divide='ignore', invalid='ignore')
-err_portion = np.divide(err.astype(float), Dist.astype(float))
-err_portion.mean()
-err
-
-
-cities
+plot()
